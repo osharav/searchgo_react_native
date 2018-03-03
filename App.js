4d2e8d6520149a4bc629347f04515777a7f5927c
Omer Sharav <osharav@gmail.com>
2018-03-03 14:39:13 +0200

    Adding a basic homepage with a map

--- App.js
+++ App.js
@@ -1,57 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import * as React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+//     Platform,
+    StyleSheet,
+    Dimensions,
+//     Text,
+    View
 } from 'react-native';
+import MapView from 'react-native-maps';
+import {TabViewAnimated, TabBar, SceneMap} from 'react-native-tab-view';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const initialLayout = {
+    height: 0,
+    width: Dimensions.get('window').width,
+};
+const FirstRoute = () => (
+    <View style={styles.container}>
+        <MapView
+            style={styles.map}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+        >
+        </MapView>
+    </View>
+);
+const SecondRoute = () => <View style={[stylesTab.container, {backgroundColor: '#673ab7'}]}/>;
 
-export default class App extends Component<{}> { 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Hola!45
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class TabViewExample extends React.Component {
+    state = {
+        index: 0,
+        routes: [
+            {key: 'first', title: 'First'},
+            {key: 'second', title: 'Second'},
+        ],
+    };
+
+    _handleIndexChange = index => this.setState({index});
+
+    _renderHeader = props => <TabBar {...props} />;
+
+    _renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+
+    render() {
+        return (
+            <TabViewAnimated
+                style={stylesTab.container}
+                navigationState={this.state}
+                renderScene={this._renderScene}
+                renderHeader={this._renderHeader}
+                onIndexChange={this._handleIndexChange}
+                initialLayout={initialLayout}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+const stylesTab = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
 });
